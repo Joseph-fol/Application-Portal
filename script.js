7218fa33b27fc55ea178d3e14fb491b710d285bb
unknown <Olawoyin Joseph Folasakin>
2026-02-10 23:11:12 +0100

    UI view of the page changed

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const info = []
-
-
-
+const info = JSON.parse(localStorage.getItem('info')) || []
 // function submit() {
 //     // alert("hello world")
 //     // details.push(firstName, lastName, gender, email, course)
 //     console.log(details);
 // }
 
-function submit() {
+
+
+function submitForm() {
     const firstName = document.getElementById('firstName').value
     const lastName = document.getElementById('lastName').value
     const gender = document.getElementById('gender').value
@@ -16,25 +15,30 @@ function submit() {
     const course = document.getElementById('course').value
 
     const errorMsg = document.getElementById("errorMessage")
-    
-    // CRITICAL: Validate BEFORE pushing data
+
+
     if (firstName == "" || lastName == "" || gender == "select" || email == "" || course == "") {
         errorMsg.style.display = "block"
     } else {
         errorMsg.style.display = "none"
-        
+
+        const matricNumber = `2026${Math.floor(Math.random() * 1000000)}`
+
         const details = {
             nameFirst: firstName,
             nameSecond: lastName,
             gender: gender,
             email: email,
-            course: course
+            course: course,
+            matricNumber: matricNumber
         };
 
         info.push(details)
+        localStorage.setItem("info", JSON.stringify(info))
+
         console.log(info);
         showDetails()
-     
+
         document.getElementById('firstName').value = ''
         document.getElementById('lastName').value = ''
         document.getElementById('gender').value = 'select'
@@ -42,6 +46,7 @@ function submit() {
         document.getElementById('course').value = ''
     }
 }
+
 
 function showDetails() {
     const tables = document.getElementById('table')
@@ -52,6 +57,7 @@ function showDetails() {
                 <th>Gender</th>
                 <th>Email</th>
                 <th>Course</th>
+                <th>Matric Number</th>
             </tr>`
 
     for (let i = 0; i < info.length; i++) {
@@ -62,6 +68,7 @@ function showDetails() {
                     <td>${info[i].gender}</td>
                     <td>${info[i].email}</td>
                     <td>${info[i].course}</td>
+                    <td>${info[i].matricNumber}</td>
                 </tr>`
     }
 }
